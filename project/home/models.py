81a518c0d39c19f5e71bb0c168b82ea4046ba3cd
afzal <afzalmomin@gmail.com>
2024-12-05 23:43:38 +0530

    added one-to-one  relation of orders model to User model

--- project/home/models.py
+++ project/home/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class orders(models.Model):
+    name = models.CharField(max_length=30)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    total = models.FloatField()
+    ordered_by = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+def __str__(self):
+    return "%s the restaurant" % self.ordered_by.username
+    pass
+
 
 class Coffee(models.Model):
     name = models.CharField(max_length=30)
